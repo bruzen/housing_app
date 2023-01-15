52a53f10cff36a3ecd4fc49c536cb1d666b774b6
bruzen <k.w.robinson@gmail.com>
2023-01-15 08:28:45 -0500

    Update streamlit_app.py
    Try combining with radio buttons and check boxes

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,3 +30,33 @@ st.write("You're scheduled for:", appointment)
 #     value=datetime(2020, 1, 1, 9, 30),
 #     format="MM/DD/YY - hh:mm")
 # st.write("Start time:", start_time)
+
+
+page_names = ['Checkbox', 'Button']
+page = st.radio('Navigation', page_names)
+st.write("**The variable 'page' returns:**", page)
+
+if page == 'Checkbox':
+   st.subheader('Welcome to the Checkbox page!')
+   st.write("Nice to see you! :wave:")
+
+   check = st.checkbox("Click here")
+   st.write('State of the checkbox:', check)
+
+   if check:
+       nested_btn = st.button("Button nested in Checkbox")
+
+       if nested_btn:
+           st.write(":cake:"*20)
+else:
+   st.subheader("Welcome to the Button page!")
+   st.write(":thumbsup:")
+
+   result = st.button('Click Here')
+   st.write("State of button:",result)
+
+   if result:
+       nested_check = st.checkbox("Checkbox nested in Button")
+
+       if nested_check:
+           st.write(":heart:"*20)
