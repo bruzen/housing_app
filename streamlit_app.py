78008a4cba45a84c055c2184363a75fc34703c07
bruzen <k.w.robinson@gmail.com>
2023-01-14 22:43:55 -0500

    Update streamlit_app.py
    Fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st. title("Making a Button"
-result = st. button ("Click Here")
-st. write (result)
+st.title("Making a Button"
+result = st.button("Click Here")
+st.write(result)
 if result:
-  st. write(": smile:")
+  st.write(":smile:")
