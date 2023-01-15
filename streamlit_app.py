024418eef4ccc358bbf63321ca7ce5378f18170d
bruzen <k.w.robinson@gmail.com>
2023-01-14 22:53:35 -0500

    Update streamlit_app.py
    Try sliders

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import time
 
 st.title("Making a Button")
 result = st.button("Click Here")
@@ -10,3 +11,22 @@ if st.button('Say hello'):
     st.write('Why hello there')
 else:
     st.write('Goodbye')
+
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
+
+values = st.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
+
+appointment = st.slider(
+    "Schedule your appointment:",
+    value=(time(11, 30), time(12, 45)))
+st.write("You're scheduled for:", appointment)
+    
+start_time = st.slider(
+    "When do you start?",
+    value=datetime(2020, 1, 1, 9, 30),
+    format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
