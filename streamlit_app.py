f1b4341cb5cde3ec3909d5421c1d5a15dfb3bba9
bruzen <k.w.robinson@gmail.com>
2023-01-15 11:22:15 -0500

    Update streamlit_app.py
    Try basketball

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,91 +1,68 @@
-# import streamlit as st
-# from datetime import time
-
-import yfinance as yf
 import streamlit as st
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
-st.write("""
-# Simple Stock Price App
+st.title('NBA Player Stats Explorer')
 
-Shown are the stock **closing price** and ***volume*** of Google!
-
+st.markdown("""
+This app performs simple webscraping of NBA player stats data!
+* **Python libraries:** base64, pandas, streamlit
+* **Data source:** [Basketball-reference.com](https://www.basketball-reference.com/).
 """)
 
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
-tickerSymbol = 'GOOGL'
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
+st.sidebar.header('User Input Features')
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2020))))
 
-st.write("""
-## Closing Price
-""")
-st.line_chart(tickerDf.Close)
-st.write("""
-## Volume Price
-""")
-st.line_chart(tickerDf.Volume)
+# Web scraping of NBA player stats
+@st.cache
+def load_data(year):
+    url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
+    html = pd.read_html(url, header = 0)
+    df = html[0]
+    raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
+    raw = raw.fillna(0)
+    playerstats = raw.drop(['Rk'], axis=1)
+    return playerstats
+playerstats = load_data(selected_year)
 
+# Sidebar - Team selection
+sorted_unique_team = sorted(playerstats.Tm.unique())
+selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
 
-# st.title("Making a Button")
-# result = st.button("Click Here")
-# st.write(result)
-# if result:
-#   st.write(":smile:")
+# Sidebar - Position selection
+unique_pos = ['C','PF','SF','PG','SG']
+selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
 
-# if st.button('Say hello'):
-#     st.write('Why hello there')
-# else:
-#     st.write('Goodbye')
+# Filtering data
+df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
 
-# age = st.slider('How old are you?', 0, 130, 25)
-# st.write("I'm ", age, 'years old')
+st.header('Display Player Stats of Selected Team(s)')
+st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
+st.dataframe(df_selected_team)
 
-# values = st.slider(
-#     'Select a range of values',
-#     0.0, 100.0, (25.0, 75.0))
-# st.write('Values:', values)
+# Download NBA player stats data
+# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+    return href
 
-# appointment = st.slider(
-#     "Schedule your appointment:",
-#     value=(time(11, 30), time(12, 45)))
-# st.write("You're scheduled for:", appointment)
-    
-# start_time = st.slider(
-#     "When do you start?",
-#     value=datetime(2020, 1, 1, 9, 30),
-#     format="MM/DD/YY - hh:mm")
-# st.write("Start time:", start_time)
+st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
 
+# Heatmap
+if st.button('Intercorrelation Heatmap'):
+    st.header('Intercorrelation Matrix Heatmap')
+    df_selected_team.to_csv('output.csv',index=False)
+    df = pd.read_csv('output.csv')
 
-# page_names = ['Checkbox', 'Button']
-# page = st.radio('Navigation', page_names)
-# st.write("**The variable 'page' returns:**", page)
-
-# if page == 'Checkbox':
-#    st.subheader('Welcome to the Checkbox page!')
-#    st.write("Nice to see you! :wave:")
-
-#    check = st.checkbox("Click here")
-#    st.write('State of the checkbox:', check)
-
-#    if check:
-#        nested_btn = st.button("Button nested in Checkbox")
-
-#        if nested_btn:
-#            st.write(":cake:"*20)
-# else:
-#    st.subheader("Welcome to the Button page!")
-#    st.write(":thumbsup:")
-
-#    result = st.button('Click Here')
-#    st.write("State of button:",result)
-
-#    if result:
-#        nested_check = st.checkbox("Checkbox nested in Button")
-
-#        if nested_check:
-#            st.write(":heart:"*20)
+    corr = df.corr()
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = True
+    with sns.axes_style("white"):
+        f, ax = plt.subplots(figsize=(7, 5))
+        ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
+    st.pyplot()
