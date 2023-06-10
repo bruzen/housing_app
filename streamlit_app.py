df29f2130f82a8bf861fc6f8fd85983d305b203f
bruzen <k.w.robinson@gmail.com>
2023-06-10 17:36:25 -0400

    Another ABM ex

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,25 +49,25 @@ class SimpleModel(Model):
     def get_total_wealth(self):
         return sum([agent.wealth for agent in self.schedule.agents])
 
-# Function to run the simulation
-def run_simulation():
-    model = SimpleModel(N=100)
-    for _ in range(100):
-        model.step()
-    
-    return model.datacollector.get_model_vars_dataframe()
-
 # Streamlit App
 def main():
     # Set page title and layout
     st.set_page_config(page_title="Agent-Based Model", layout="wide")
     
     # Set app title and description
-    st.title("Agent-Based Model with Streamlit Cloud")
-    st.write("This is a simple example of an agent-based model displayed using Streamlit Cloud.")
+    st.title("Agent-Based Model with Streamlit")
+    st.write("This is a simple example of an agent-based model displayed using Streamlit.")
+    
+    # User input for simulation parameters
+    num_agents = st.slider("Number of Agents", min_value=10, max_value=100, value=50)
+    num_steps = st.slider("Number of Steps", min_value=10, max_value=200, value=100)
     
     # Run the simulation and retrieve data
-    data = run_simulation()
+    model = SimpleModel(N=num_agents)
+    for _ in range(num_steps):
+        model.step()
+    
+    data = model.datacollector.get_model_vars_dataframe()
     
     # Display data using Streamlit components
     st.header("Model Output")
