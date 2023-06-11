327dd05d8f467f66cd57218bd2ccbacb074c6133
bruzen <k.w.robinson@gmail.com>
2023-06-10 23:45:58 -0400

    Try a different ABM

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,85 +1,53 @@
-import streamlit as st
+import numpy as np
 from mesa import Agent, Model
 from mesa.time import RandomActivation
-from mesa.space import SingleGrid
+from mesa.space import MultiGrid
 from mesa.datacollection import DataCollector
+import streamlit as st
 
-class SimpleAgent(Agent):
+# Define the Agent class
+class MyAgent(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.wealth = 1
-    
-    def step(self):
-        self.move()
-        self.interact()
-    
-    def move(self):
-        possible_steps = self.model.grid.get_neighborhood(
-            self.pos, moore=True, include_center=False)
-        new_position = self.random.choice(possible_steps)
-        self.model.grid.move_agent(self, new_position)
-    
-    def interact(self):
-        cellmates = self.model.grid.get_cell_list_contents([self.pos])
-        if len(cellmates) > 1:
-            other_agent = self.random.choice(cellmates)
-            self.wealth += 1
-            other_agent.wealth -= 1
 
-class SimpleModel(Model):
-    def __init__(self, N, width, height):
-        self.num_agents = N
+    def step(self):
+        neighbors = self.model.grid.get_neighbors(self.pos)
+        if neighbors:
+            other_agent = self.random.choice(neighbors)
+            self.model.grid.move_agent(self, other_agent.pos)
+
+# Define the Model class
+class MyModel(Model):
+    def __init__(self, width, height, num_agents):
+        self.width = width
+        self.height = height
+        self.num_agents = num_agents
         self.schedule = RandomActivation(self)
-        self.grid = SingleGrid(width, height, torus=True)
-        
+        self.grid = MultiGrid(self.width, self.height, torus=True)
+
         for i in range(self.num_agents):
-            a = SimpleAgent(i, self)
-            self.schedule.add(a)
-            x = self.random.randrange(self.grid.width)
-            y = self.random.randrange(self.grid.height)
-            self.grid.place_agent(a, (x, y))
-        
-        self.datacollector = DataCollector(
-            model_reporters={"Wealth": lambda m: self.get_total_wealth()})
-    
+            agent = MyAgent(i, self)
+            self.schedule.add(agent)
+            x = self.random.randrange(self.width)
+            y = self.random.randrange(self.height)
+            self.grid.place_agent(agent, (x, y))
+
+        self.datacollector = DataCollector(model_reporters={"NumAgents": lambda m: m.schedule.get_agent_count()})
+
     def step(self):
         self.datacollector.collect(self)
         self.schedule.step()
-    
-    def get_total_wealth(self):
-        return sum([agent.wealth for agent in self.schedule.agents])
 
-# Streamlit App
-def main():
-    # Set page title and layout
-    st.set_page_config(page_title="Agent-Based Model", layout="wide")
-    
-    # Set app title and description
-    st.title("Agent-Based Model with Streamlit")
-    st.write("This is a simple example of an agent-based model displayed using Streamlit.")
-    
-    # User input for simulation parameters
-    num_agents = st.slider("Number of Agents", min_value=10, max_value=100, value=50)
-    grid_size = st.slider("Grid Size", min_value=10, max_value=50, value=20)
-    num_steps = st.slider("Number of Steps", min_value=10, max_value=200, value=100)
-    
-    # Run the simulation and retrieve data
-    model = SimpleModel(N=num_agents, width=grid_size, height=grid_size)
+# Streamlit app
+def run_model(num_steps):
+    model = MyModel(10, 10, 100)
     for _ in range(num_steps):
         model.step()
-    
-    data = model.datacollector.get_model_vars_dataframe()
-    
-    # Display data using Streamlit components
-    st.header("Model Output")
-    st.dataframe(data)
-    
-    st.header("Visualization")
-    # Add visualization components here (e.g., matplotlib, plotly, etc.)
-    # You can create plots or charts to visualize the model output.
-    # For example, you can use st.pyplot() to display matplotlib plots.
-    # Customize the visualization as per your specific needs.
-    
-# Run the app
-if __name__ == '__main__':
-    main()
+
+    # Display the number of agents using Streamlit
+    st.line_chart(model.datacollector.get_model_vars_dataframe())
+
+# Run the Streamlit app
+if __name__ == "__main__":
+    num_steps = st.sidebar.slider("Number of Steps", 0, 100, 10)
+    run_model(num_steps)
