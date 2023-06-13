fe6ae559156ad11eadcb2f73102e18600f139cd2
bruzen <k.w.robinson@gmail.com>
2023-06-13 14:05:20 -0400

    Pass parameter values to City

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,37 @@ import matplotlib.pyplot as plt
 from model import City  # Import the City model from model.py
 
 def run_model(num_steps):
-    # Create and run the model
-    city = City()
+    # Create and run the model with the specified parameter values
+    city = City(
+        width=50,
+        height=1,
+        subsistence_wage=40000.,
+        working_periods=40,
+        savings_rate=0.3,
+        r_prime=0.05,
+        r_margin=0.01,
+        prefactor=250,
+        agglomeration_ratio=1.2,
+        workers_share=0.72,
+        property_tax_rate=0.04,
+        mortgage_period=5.0,
+        housing_services_share=0.3,
+        maintenance_share=0.2,
+        seed_population=0,
+        density=100,
+        max_mortgage_share=0.9,
+        ability_to_carry_mortgage=0.28,
+        wealth_sensitivity=0.1,
+        wage_adjust_coeff_new_workers=0.5,
+        wage_adjust_coeff_exist_workers=0.5,
+        workforce_rural_firm=100,
+        price_of_output=1.,
+        beta_city=1.12,
+        alpha_firm=0.18,
+        z=0.5,
+        init_wage_premium_ratio=0.2,
+        init_city_extent=10.
+    )
     for t in range(num_steps):
         city.step()
 
