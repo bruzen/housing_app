d1f56c675f7e2f2df2842043ba1465f0dc6aeeeb
bruzen <k.w.robinson@gmail.com>
2023-06-13 21:30:11 -0400

    Remove duplicate sliders now model works

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,18 +51,8 @@ def run_model(num_steps, subsistence_wage, working_periods, savings_rate, r_prim
     ax[1, 1].set_title('subplot 4')
 
     # Display the plots using Streamlit
-    col1, col2 = st.columns(2)
-
-    with col1:
-        st.title("Agent-Based Model Visualization")
-        num_steps = st.slider("Number of Steps", key="num_steps", min_value=1, max_value=100, value=num_steps)
-        subsistence_wage = st.slider("Subsistence Wage", key="subsistence_wage", min_value=30000., max_value=50000., value=subsistence_wage, step=1000.)
-        working_periods = st.slider("Working Periods", key="working_periods", min_value=30, max_value=50, value=working_periods)
-        savings_rate = st.slider("Savings Rate", key="savings_rate", min_value=0.1, max_value=0.5, value=savings_rate, step=0.05)
-        r_prime = st.slider("R Prime", key="r_prime", min_value=0.03, max_value=0.07, value=r_prime, step=0.01)
-    
-    with col2:
-        st.pyplot(fig)
+    st.title("Agent-Based Model Visualization")
+    st.pyplot(fig)
 
 def main():
     st.title("Agent-Based Model Visualization")
