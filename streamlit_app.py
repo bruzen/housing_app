91768681ed1aadd2a93ff1097c888f1cc78c1e26
k <k.w.robinson@gmail.com>
2023-07-04 12:27:12 -0400

    Update Streamlit app to pass parameters as a dictionary

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,46 +6,9 @@ import matplotlib.pyplot as plt
 
 from model.model import City
 
-def run_model(num_steps, subsistence_wage, working_periods, savings_rate, r_prime):
-    width = 50
-    height = 1
+def run_model(parameters, num_steps):
 
-
-    # parameters = {
-    #     'width': 50,
-    #     'height': 1,
-    #     'init_city_extent': 10.,
-    #     'seed_population': 10,
-    #     'density': 100,
-    #     'subsistence_wage': 40000.,
-    #     'init_wage_premium_ratio': 0.2,
-    #     'workforce_rural_firm': 100,
-    #     'price_of_output': 1.,
-    #     'alpha_F': 0.18,
-    #     'beta_F': 0.72,
-    #     'beta_city': 1.12,
-    #     'gamma': 0.02,
-    #     'Z': 0.5,
-    #     'firm_adjustment_parameter': 0.25,
-    #     'wage_adjustment_parameter': 0.5,
-    #     'mortgage_period': 5.0,
-    #     'working_periods': 40,
-    #     'savings_rate': 0.3,
-    #     'r_prime': 0.05,
-    #     'r_margin': 0.01,
-    #     'property_tax_rate': 0.04,
-    #     'housing_services_share': 0.3,
-    #     'maintenance_share': 0.2,
-    #     'max_mortgage_share': 0.9,
-    #     'ability_to_carry_mortgage': 0.28,
-    #     'wealth_sensitivity': 0.1,
-    # }
-
-    # city = City(parameters)
-
-    # Create and run the model
-    city = City(width=width, height=height, subsistence_wage=subsistence_wage, working_periods=working_periods,
-                savings_rate=savings_rate, r_prime=r_prime)
+    city = City(parameters)
 
     for t in range(num_steps):
         city.step()
@@ -113,15 +76,19 @@ def run_model(num_steps, subsistence_wage, working_periods, savings_rate, r_prim
     st.header("Explore Existing Data")
     st.pyplot(fig)
 
-
 def main():
     num_steps = st.sidebar.slider("Number of Steps", min_value=1, max_value=100, value=50)
-    subsistence_wage = st.sidebar.slider("Subsistence Wage", min_value=30000., max_value=50000., value=40000., step=1000.)
-    working_periods = st.sidebar.slider("Working Periods", min_value=30, max_value=50, value=40)
-    savings_rate = st.sidebar.slider("Savings Rate", min_value=0.1, max_value=0.5, value=0.3, step=0.05)
-    r_prime = st.sidebar.slider("R Prime", min_value=0.03, max_value=0.07, value=0.05, step=0.01)
 
-    run_model(num_steps, subsistence_wage, working_periods, savings_rate, r_prime)
+    parameters = {
+        'width': 50,
+        'height': 1,
+        'subsistence_wage': st.sidebar.slider("Subsistence Wage", min_value=30000., max_value=50000., value=40000., step=1000.),
+        'working_periods': st.sidebar.slider("Working Periods", min_value=30, max_value=50, value=40),
+        'savings_rate': st.sidebar.slider("Savings Rate", min_value=0.1, max_value=0.5, value=0.3, step=0.05),
+        'r_prime': st.sidebar.slider("R Prime", min_value=0.03, max_value=0.07, value=0.05, step=0.01)
+    }
+
+    run_model(parameters, num_steps) # num_steps, subsistence_wage, working_periods, savings_rate, r_prime)
 
 if __name__ == "__main__":
     main()
