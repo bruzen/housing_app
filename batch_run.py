ad22956bbbb07b34de12cb0e35c8828eac8e4e72
k <k.w.robinson@gmail.com>
2023-07-14 12:58:28 -0400

    Pass fixed parameters and put batch run data in batch run folders

--- batch_run.py
+++ batch_run.py
@@ -12,7 +12,42 @@ variable_parameters = {
     'subsistence_wage': [10000, 30000]
 }
 
-other_parameters = {
+fixed_parameters = {
+    'subfolder': None,
+    'run_notes': 'Debugging model.',
+    'width': 50,
+    'height': 1,
+    'init_city_extent': 10.,  # f CUT OR CHANGE?
+    'seed_population': 10,
+    'density': 300,
+    'subsistence_wage': 40000.,  # psi
+    'init_wage_premium_ratio': 0.2,
+    'workforce_rural_firm': 100,
+    'price_of_output': 1.,  # TODO CUT?
+    'alpha_F': 0.18,
+    'beta_F': 0.72,  # beta and was lambda, workers_share of aglom surplus
+    'beta_city': 1.12,
+    'gamma': 0.02,  # FIX value
+    'Z': 0.5,  # CUT? Scales new entrants
+    'firm_adjustment_parameter': 0.25,
+    'wage_adjustment_parameter': 0.5,
+    'mortgage_period': 5.0,  # T, in years
+    'working_periods': 40,  # in years
+    'savings_rate': 0.3,
+    'r_prime': 0.05,  # 0.03
+    'discount_rate': 0.07, # 1/delta
+    'r_margin': 0.01,
+    'property_tax_rate': 0.04,  # tau, annual rate, was c
+    'housing_services_share': 0.3,  # a
+    'maintenance_share': 0.2,  # b
+    'max_mortgage_share': 0.9,
+    'ability_to_carry_mortgage': 0.28,
+    'wealth_sensitivity': 0.1,
+}
+
+
+
+batch_parameters = {
     'data_collection_period': 2,
     'iterations': 1,
     'max_steps': 3
@@ -20,22 +55,21 @@ other_parameters = {
 
 # Define the context manager to record metadata
 @contextmanager
-def metadata_recorder(parameters, other_parameters):
+def metadata_recorder(model_parameters, batch_parameters):
     metadata = {
-        'variable_parameters': parameters,
-        'other_parameters': other_parameters
+        'model_parameters': model_parameters,
+        'batch_parameters': batch_parameters
     }
     yield metadata
     # Save the metadata to a YAML file
     metadata_path = os.path.join(subfolder, 'batch_metadata.yaml')
     with open(metadata_path, 'w') as f:
         yaml.safe_dump(metadata, f)
-
     
 # Define the function to run the batch simulation
 def run_batch_simulation():
     # Run the batch simulations
-    results = batch_run(City, variable_parameters, **other_parameters)
+    results = batch_run(City, model_parameters, **batch_parameters)
     df = pd.DataFrame(results)
     # df.to_csv('batch_results.csv', index=False)
     df.to_csv(os.path.join(subfolder, 'batch_results.csv'), index=False)
@@ -54,5 +88,7 @@ def get_subfolder():
 # Main execution
 if __name__ == '__main__':
     subfolder = get_subfolder()
-    with metadata_recorder(variable_parameters, other_parameters):
+    fixed_parameters['subfolder'] = subfolder
+    model_parameters = {**fixed_parameters, **variable_parameters}
+    with metadata_recorder(model_parameters, batch_parameters):
         run_batch_simulation()
