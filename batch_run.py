69d18d7a35d2cd4813d3442d0e8be4f11531dfe6
k <k.w.robinson@gmail.com>
2023-07-14 04:12:33 -0400

    Save batch_run output to a subfolder

--- batch_run.py
+++ batch_run.py
@@ -1,6 +1,7 @@
 import sys
 import yaml
 import pandas as pd
+import os
 from contextlib import contextmanager
 from mesa.batchrunner import batch_run
 from model.model import City
@@ -14,7 +15,7 @@ variable_parameters = {
 other_parameters = {
     'data_collection_period': 2,
     'iterations': 1,
-    'max_steps': 30
+    'max_steps': 3
 }
 
 # Define the context manager to record metadata
@@ -26,18 +27,32 @@ def metadata_recorder(parameters, other_parameters):
     }
     yield metadata
     # Save the metadata to a YAML file
-    with open('metadata_batch_run.yaml', 'w') as f:
+    metadata_path = os.path.join(subfolder, 'batch_metadata.yaml')
+    with open(metadata_path, 'w') as f:
         yaml.safe_dump(metadata, f)
 
+    
 # Define the function to run the batch simulation
 def run_batch_simulation():
     # Run the batch simulations
     results = batch_run(City, variable_parameters, **other_parameters)
     df = pd.DataFrame(results)
-    df.to_csv('batch_results.csv', index=False)
+    # df.to_csv('batch_results.csv', index=False)
+    df.to_csv(os.path.join(subfolder, 'batch_results.csv'), index=False)
+
+def get_subfolder():
+    # Create the subfolder path
+    output_data_folder = 'output_data'
+    runs_folder = 'batch_runs'
+    subfolder = os.path.join(output_data_folder, runs_folder)
+
+    # Create the subfolder if it doesn't exist
+    os.makedirs(subfolder, exist_ok=True)
+
+    return subfolder
 
 # Main execution
 if __name__ == '__main__':
-
+    subfolder = get_subfolder()
     with metadata_recorder(variable_parameters, other_parameters):
         run_batch_simulation()
