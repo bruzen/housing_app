6f513cc840fbacdff39e3c9e540acf78393d2297
k <k.w.robinson@gmail.com>
2023-07-15 14:35:27 -0400

    Simplify test and plot all lines, give an hourglass

--- sstreamlit_test.py
+++ sstreamlit_test.py
@@ -2,7 +2,6 @@ import os
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import yaml
 
 def get_batch_run_folders():
     output_data_folder = "output_data"
@@ -11,26 +10,16 @@ def get_batch_run_folders():
     batch_run_folders = [folder for folder in batch_run_folders if folder != ".DS_Store"]
     return batch_run_folders
 
-def get_batch_run_keys(folder_path):
-    file_names = os.listdir(folder_path)
-    keys = []
+def load_data(folder_path):
+    model_files = [f for f in os.listdir(folder_path) if f.endswith("_model.csv")]
+    data = pd.DataFrame()
 
-    for file_name in file_names:
-        if file_name.endswith("_model.csv"):
-            key = file_name.replace("_model.csv", "")
-            keys.append(key)
+    for file_name in model_files:
+        file_path = os.path.join(folder_path, file_name)
+        run_data = pd.read_csv(file_path)
+        data = data.append(run_data, ignore_index=True)
 
-    return keys
-
-def load_data(run_id, folder_path):
-    model_file = os.path.join(folder_path, f"{run_id}_model.csv")
-
-    if os.path.exists(model_file):
-        model_data = pd.read_csv(model_file)
-        return model_data
-    else:
-        st.error(f"Data file not found for run ID: {run_id}")
-        return None
+    return data
 
 def plot_data(data):
     plt.plot(data['time_step'], data['wage'])
@@ -39,37 +28,14 @@ def plot_data(data):
     plt.title('Wage vs Step')
     st.pyplot()
 
-def load_metadata(folder_path):
-    metadata_file = os.path.join(folder_path, "run_metadata.yaml")
-
-    if os.path.exists(metadata_file):
-        with open(metadata_file, 'r') as file:
-            metadata = yaml.safe_load(file)
-        return metadata
-    else:
-        st.warning("Metadata file not found.")
-        return None
-
 def main():
     st.title("Batch Run Data Plotter")
     batch_run_folders = get_batch_run_folders()
     selected_folder = st.selectbox("Select Batch Run Folder", batch_run_folders)
     folder_path = os.path.join("output_data", "batch_runs", selected_folder)
 
-    run_ids = get_batch_run_keys(folder_path)
-    selected_run_id = st.selectbox("Select Run ID", run_ids)
-
-    data = load_data(selected_run_id, folder_path)
-    if data is not None:
-        st.subheader("Data")
-        st.dataframe(data)
-
-        metadata = load_metadata(folder_path)
-        if metadata is not None:
-            parameters = metadata[selected_run_id]['simulation_parameters']
-            st.subheader("Metadata")
-            st.write(parameters)
-
+    data = load_data(folder_path)
+    if not data.empty:
         st.subheader("Plot")
         plot_data(data)
 
