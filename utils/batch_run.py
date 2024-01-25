808fda18f8a7a1c29308e9ba0079266ea086569e
k <k.w.robinson@gmail.com>
2024-01-24 23:21:40 -0500

    Move parameters to main execution for batch_run

--- utils/batch_run.py
+++ utils/batch_run.py
@@ -9,75 +9,6 @@ from contextlib import contextmanager
 from mesa.batchrunner import batch_run
 from model.model import City
 
-batch_parameters = {
-            'data_collection_period': 1,
-            'iterations': 1,
-            'max_steps': 5
-}
-
-# Define the variable and fixed parameters
-variable_parameters = {
-    'density': [1, 100],
-    # 'subsistence_wage': [10000, 30000],
-    'gamma': [0.02]
-}
-
-fixed_parameters = {
-            'run_notes': 'Debugging model.',
-            'subfolder': None,
-            'width':     10, #30,
-            'height':    10, #30,
-
-            # FLAGS
-            'demographics_on': True,  # Set flag to False for debugging to check firm behaviour without demographics or housing market
-            'center_city':     False, # Flag for city center in center if True, or bottom corner if False
-            # 'random_init_age': False,  # Flag for randomizing initial age. If False, all workers begin at age 0
-            'random_init_age': True,  # Flag for randomizing initial age. If False, all workers begin at age 0
-
-            # LABOUR MARKET AND FIRM PARAMETERS
-            'subsistence_wage': 40000., # psi
-            'init_city_extent': 10.,    # CUT OR CHANGE?
-            'seed_population': 400,
-            'init_wage_premium_ratio': 0.2, # 1.2, ###
-
-            # PARAMETERS MOST LIKELY TO AFFECT SCALE
-            'c': 300.0,                            ###
-            'price_of_output': 10,                 ######
-            'density': 600,                         #####
-            'A': 3000,                             ### 
-            'alpha': 0.18,
-            'beta':  0.75,
-            'gamma': 0.12, ### reduced from .14
-            'overhead': 1,
-            'mult': 1.2,
-            'adjN': 0.15,
-            'adjk': 0.10,
-            'adjn': 0.25,
-            'adjF': 0.15,
-            'adjw': 0.02, 
-            'dist': 1, 
-            'init_F': 100.0,
-            'init_k': 500.0,
-            'init_n': 100.0,
-
-            # HOUSING AND MORTGAGE MARKET PARAMETERS
-            'mortgage_period': 5.0,       # T, in years
-            'working_periods': 40,        # in years
-            'savings_rate': 0.3,
-            'discount_rate': 0.07,        # 1/delta
-            'r_prime': 0.05,
-            'r_margin': 0.01,
-            'r_investor': 0.05,            # Next best alternative return for investor
-            'property_tax_rate': 0.04,     # tau, annual rate, was c
-            'housing_services_share': 0.3, # a
-            'maintenance_share': 0.2,      # b
-            'max_mortgage_share': 0.9,
-            'ability_to_carry_mortgage': 0.28,
-            'wealth_sensitivity': 0.1,
-            'cg_tax_per':   0.01, # share 0-1
-            'cg_tax_invest': 0.15, # share 0-1
-        }
-
 @contextmanager
 def metadata_recorder(batch_parameters, variable_parameters, fixed_parameters, subfolder, name = None):
     metadata = {
@@ -265,6 +196,74 @@ def run_experiment(batch_parameters, variable_parameters, fixed_parameters, name
 
 # Main execution
 if __name__ == '__main__':
+    batch_parameters = {
+                'data_collection_period': 1,
+                'iterations': 1,
+                'max_steps': 5
+    }
+
+    # Define the variable and fixed parameters
+    variable_parameters = {
+        'density': [1, 100],
+        # 'subsistence_wage': [10000, 30000],
+        'gamma': [0.02]
+    }
+
+    fixed_parameters = {
+        'run_notes': 'Debugging model.',
+        'subfolder': None,
+        'width':     10, #30,
+        'height':    10, #30,
+
+        # FLAGS
+        'demographics_on': True,  # Set flag to False for debugging to check firm behaviour without demographics or housing market
+        'center_city':     False, # Flag for city center in center if True, or bottom corner if False
+        # 'random_init_age': False,  # Flag for randomizing initial age. If False, all workers begin at age 0
+        'random_init_age': True,  # Flag for randomizing initial age. If False, all workers begin at age 0
+
+        # LABOUR MARKET AND FIRM PARAMETERS
+        'subsistence_wage': 40000., # psi
+        'init_city_extent': 10.,    # CUT OR CHANGE?
+        'seed_population': 400,
+        'init_wage_premium_ratio': 0.2, # 1.2, ###
+
+        # PARAMETERS MOST LIKELY TO AFFECT SCALE
+        'c': 300.0,                            ###
+        'price_of_output': 10,                 ######
+        'density': 600,                         #####
+        'A': 3000,                             ### 
+        'alpha': 0.18,
+        'beta':  0.75,
+        'gamma': 0.12, ### reduced from .14
+        'overhead': 1,
+        'mult': 1.2,
+        'adjN': 0.15,
+        'adjk': 0.10,
+        'adjn': 0.25,
+        'adjF': 0.15,
+        'adjw': 0.02, 
+        'dist': 1, 
+        'init_F': 100.0,
+        'init_k': 500.0,
+        'init_n': 100.0,
+
+        # HOUSING AND MORTGAGE MARKET PARAMETERS
+        'mortgage_period': 5.0,       # T, in years
+        'working_periods': 40,        # in years
+        'savings_rate': 0.3,
+        'discount_rate': 0.07,        # 1/delta
+        'r_prime': 0.05,
+        'r_margin': 0.01,
+        'r_investor': 0.05,            # Next best alternative return for investor
+        'property_tax_rate': 0.04,     # tau, annual rate, was c
+        'housing_services_share': 0.3, # a
+        'maintenance_share': 0.2,      # b
+        'max_mortgage_share': 0.9,
+        'ability_to_carry_mortgage': 0.28,
+        'wealth_sensitivity': 0.1,
+        'cg_tax_per':   0.01, # share 0-1
+        'cg_tax_invest': 0.15, # share 0-1
+            }
     fixed_parameters['timestamp'] = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")
     subfolder = get_subfolder(fixed_parameters['timestamp'], variable_parameters)
     fixed_parameters['subfolder'] = subfolder
