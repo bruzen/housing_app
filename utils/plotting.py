87060264f968f8df66cb85f3629bdce18f80f28c
k <k.w.robinson@gmail.com>
2024-01-27 22:56:11 -0500

    Simplify plots

--- utils/plotting.py
+++ utils/plotting.py
@@ -18,7 +18,7 @@ def set_style():
 
 def small_multiples_lineplot(df, param_mapping, palette=None):
     set_style()
-    plt.rcParams['font.size'] = 12
+    plt.rcParams['font.size'] = 10
 
     # Desired width (thesis) and space (sns default) values
     width = PAGE_WIDTH  # inches, minus 1 inch for the legend
@@ -48,7 +48,7 @@ def small_multiples_lineplot(df, param_mapping, palette=None):
     # Show the plot
     plt.show()
 
-def batch_quantities_lineplot(df, variable_parameters = None):
+def variables_vs_time(df, variable_parameters = None):
     set_style()
     plt.rcParams['font.size'] = 11
 
@@ -61,12 +61,12 @@ def batch_quantities_lineplot(df, variable_parameters = None):
     cmap       = plt.get_cmap('tab10')
     num_runs   = len(df['RunId'].unique())
     colors     = [cmap(i) for i in np.linspace(0, 1, num_runs)]
-    linewidths = [1, 2, 3, 4]
+    linewidths = [.5, 1, 1.5, 2] # linewidths = [1, 2, 3, 4]
     linestyles = ['solid', 'dashed', 'dashdot', 'dotted']  # Add more if needed
     alpha      = 0.8   
 
     # Create subplots with a 4x2 grid
-    fig, axes = plt.subplots(4, 2, figsize=(PAGE_WIDTH, 1.5*PAGE_WIDTH), gridspec_kw={'hspace': 0.8, 'wspace': 0.5})  # 4 rows, 2 columns
+    fig, axes = plt.subplots(4, 2, figsize=(PAGE_WIDTH/GOLDEN_RATIO, PAGE_WIDTH), gridspec_kw={'hspace': 1.4, 'wspace': 0.6})  # 4 rows, 2 columns
 
     # Loop through each run
     for i, run_id in enumerate(df['RunId'].unique()):
@@ -89,62 +89,68 @@ def batch_quantities_lineplot(df, variable_parameters = None):
 
         # Plot MPL
         axes[0, 0].plot(subset_df['time_step'], subset_df['MPL'], label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        axes[0, 0].set_xlabel('Time Step')
+        # axes[0, 0].set_xlabel('Time Step')
         axes[0, 0].set_ylabel('MPL')
-        axes[0, 0].set_title(f'MPL over time')
+        # axes[0, 0].set_title(f'MPL')
         axes[0, 0].grid(True)
         axes[0, 0].legend().set_visible(False)
 
         # Plot n
         axes[0, 1].plot(subset_df['time_step'], subset_df['n'], label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        axes[0, 1].set_xlabel('Time Step')
+        # axes[0, 1].set_xlabel('Time Step')
         axes[0, 1].set_ylabel('n')
-        axes[0, 1].set_title(f'Urban firm workforce n over time')
+        # axes[0, 1].set_title(f'Firm workforce') # (n)')
         axes[0, 1].grid(True)
         axes[0, 1].legend().set_visible(False)
 
         # Plot N
         axes[1, 0].plot(subset_df['time_step'], subset_df['N'], label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        axes[1, 0].set_xlabel('Time Step')
+        # axes[1, 0].set_xlabel('Time Step')
         axes[1, 0].set_ylabel('N')
-        axes[1, 0].set_title(f'Total urban workforce over time')
+        # axes[1, 0].set_title(f'Total workforce') # (N)')
         axes[1, 0].grid(True)
         axes[1, 0].legend().set_visible(False)
 
         # Plot F
         axes[1, 1].plot(subset_df['time_step'], subset_df['F'], label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        axes[1, 1].set_xlabel('Time Step')
+        # axes[1, 1].set_xlabel('Time Step')
         axes[1, 1].set_ylabel('F')
-        axes[1, 1].set_title(f'Number of firms over time')
+        # axes[1, 1].set_title(f'Number of firms') # (F)')
         axes[1, 1].grid(True)
         axes[1, 1].legend().set_visible(False)
 
         # Plot city extent
         axes[2, 0].plot(subset_df['time_step'], subset_df['city_extent_calc'], label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        axes[2, 0].set_xlabel('Time Step')
-        axes[2, 0].set_ylabel('Lot widths')
-        axes[2, 0].set_title(f'Calculated city extent over time')
+        # axes[2, 0].set_xlabel('Time Step')
+        axes[2, 0].set_ylabel('Properties')
+        # axes[2, 0].set_title(f'City extent')
         axes[2, 0].grid(True)
         axes[2, 0].legend().set_visible(False)
 
         # Plot 'k'
         axes[2, 1].plot(subset_df['time_step'], subset_df['k'], label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        axes[2, 1].set_xlabel('Time Step')
+        # axes[2, 1].set_xlabel('Time Step')
         axes[2, 1].set_ylabel('k')
-        axes[2, 1].set_title(f'Urban firm capital over time')
+        # axes[2, 1].set_title(f'Firm capital') # (k)')
         axes[2, 1].grid(True)
         axes[2, 1].legend().set_visible(False)
 
-        # Plot 'investor_ownership_share'
+        # Plot 'Owner-occupier_share'
         axes[3, 0].plot(subset_df['time_step'], (1- subset_df['investor_ownership_share']), label=label, color=color, alpha=alpha, linestyle=linestyle, linewidth=linewidth)
-        axes[3, 0].set_xlabel('Time Step')
-        axes[3, 0].set_ylabel('Ownership share')
-        axes[3, 0].set_title('Owner-occupier fraction over time')
+        # axes[3, 0].set_xlabel('Time Step')
+        axes[3, 0].set_ylabel('Owner-occupier share') #('Ownership share')
+        # axes[3, 0].set_title('Owner-occupier') #('Owner-occupier fraction')
         axes[3, 0].grid(True)
 
         # Display a single legend outside the figure
-        axes[3, 0].legend(loc='center left', bbox_to_anchor=(1.35, 0.5), frameon=False)
+        axes[3, 0].legend(loc='center left', bbox_to_anchor=(1.2, 0.5), frameon=False)
         axes[3, 1].set_axis_off() 
+
+    # for ax_row in axes:
+    # for ax in ax_row:
+    #     ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True, useOffset=False))
+    #     ax.ticklabel_format(style='plain', axis='y')
+
 
     # Override font sizes
     default_font_size = plt.rcParams['font.size']
@@ -153,11 +159,13 @@ def batch_quantities_lineplot(df, variable_parameters = None):
         ax.set_xlabel(ax.get_xlabel(), fontsize=default_font_size)
         ax.set_ylabel(ax.get_ylabel(), fontsize=default_font_size)
 
-    figure_filepath = file_utils.get_figures_filepath(f'timeseries-plots-{timestamp}.pdf')
-
+    name = f'{" ".join(variable_parameters.keys())}-{timestamp}'
+    figure_filepath = file_utils.get_figures_filepath(f'{name}.pdf')
     label_text = (
+        # name
+        f'{timestamp}\n'
         # f'\n {name} {" ".join(variable_parameters.keys())}'
-        f'{figure_filepath}\n'
+        # f'{figure_filepath}\n'
         # f'adjF: {model_parameters["adjF"]}, adjw: {model_parameters["adjw"]}, '
         # f'discount_rate: {model_parameters["discount_rate"]}, r_margin: {model_parameters["r_margin"]},\n'
         # f'max_mortgage_share: {model_parameters["max_mortgage_share"]}, '
@@ -165,8 +173,8 @@ def batch_quantities_lineplot(df, variable_parameters = None):
         # f'cg_tax_invest: {model_parameters["cg_tax_invest"]}'
     )
 
-    plt.text(-1.0, -0.7, label_text, transform=plt.gca().transAxes, ha='left', va='center', wrap=True)
-    plt.savefig(figure_filepath, format='pdf')
+    plt.text(-1.3, -.9, label_text, transform=plt.gca().transAxes, ha='left', va='center', wrap=True)
+    plt.savefig(figure_filepath, format='pdf', bbox_inches='tight')
 
 def format_label(label):
     # Capitalize the first letter of each word
